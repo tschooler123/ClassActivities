e52e0df9374fd1c07263d49e33d7a7524e911f33
tschooler123 <tshaylaschooler@gmail.com>
2023-03-02 20:47:36 +0100

    Switch Case Statement

--- Section02/2.3/2.3.3-SwitchStatements.js
+++ Section02/2.3/2.3.3-SwitchStatements.js
@@ -7,35 +7,39 @@ let dayName = `Today`;
 // Complete this switch case statement to produce the correct results please.
 
 switch (day) {
-  case **{`Which case goes here`}** :
+  case 4: {
     dayName = `Today is Wednesday`;
     break;
-  case 1:
-    dayName = `Today is **{what day goes here}**`;
+  }
+
+  case 1:{
+    dayName = `Today is Monday.`;
     break;
+  }
 
   // Note the code blocks in the next 2 cases: Why?
   case 4: {
-    const message = `Today is **{what day goes here}**`;
+    const message = `Today is Wednesday.`;
     console.log(message);
     break;
   }
-  case **{`Which case goes here`}** : {
+  case 6 : {
     const message = `Tomorrow it's the weekend!`;
     console.log(message);
     break;
   }
   default:
-    console.log(**{`Write a sassy default message here`}**);
+    console.log(`The weekdays are my favorite`);
     break;
-  case **{`Which case goes here`}** :
+
+  case 2:
     console.log(`Today is Tuesday`);
     break;
   // write a case here for the weekend (how many days in the weekend?)
-  case :
+  case 6 :
     alert(`It's the Weekend !!`);
 
 }
-if (dayName !== 'Today'){
+if (dayName !== 'Today') {
 console.log(dayName);
 }
